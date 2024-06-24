4a6800c2fd7731b347e701a7b2a36c7acffce1d8
kim-tiffany <150105886+kim-tiffany@users.noreply.github.com>
2024-06-24 11:29:13 -0700

    update 1

---
+++ src/controllers/userControllers.js
@@ -0,0 +1,6 @@
+export const join = (req, res) => res.send("Join");
+export const edit = (req, res) => res.send("Edit User");
+export const remove = (req, res) => res.send("Remove User");
+export const login = (re, res) => res.send("Login");
+export const logout = (req, res) => res.send("Logout");
+export const see = (res, res) => res.send("See user");
---
+++ src/server.js
@@ -0,0 +1,19 @@
+import express from "express";
+import morgan from "morgan";
+import globalRouter from "./routers/globalRouter";
+import userRouter from "./routers/userRouter";
+import videoRouter from "./routers/videoRouter";
+
+const PORT = 5555;
+const app = express();
+const logger = morgan("dev");
+app.use(logger);
+
+app.use("/", globalRouter);
+app.use("/videos", videoRouter);
+app.use("/users", userRouter);
+
+const handleListening = () =>
+    console.log(`Server listening on port http://localhost:${PORT} 🥰`);
+
+app.listen(PORT, handleListening);

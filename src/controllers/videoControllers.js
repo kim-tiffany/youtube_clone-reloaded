5e571146ba43a6a9500fabd8814aeeb16e0e4019
kim-tiffany <150105886+kim-tiffany@users.noreply.github.com>
2024-07-16 12:44:25 -0700

    update 7

--- src/controllers/videoControllers.js
+++ src/controllers/videoControllers.js
@@ -1,26 +1,45 @@
 import Video from "../models/Video";
 
-export const home = (req, res) => {
-    Video.find({}, (error, videos) => {});
-    res.render("home", { pageTitle: "Home" });
+export const home = async (req, res) => {
+    const videos = await Video.find({});
+    return res.render("home", { pageTitle: "Home", videos });
 };
-export const watch = (req, res) => {
+export const watch = async (req, res) => {
     const { id } = req.params;
-
+    const video = await Video.findById(id);
+    if (!video) {
+        return res.render("404", { pageTitle: "Video not found" });
+    }
     return res.render("watch", {
-        pageTitle: `Watching`,
+        pageTitle: video.title,
+        video,
     });
 };
 
-export const getEdit = (req, res) => {
+export const getEdit = async (req, res) => {
     const { id } = req.params;
+    const video = await Video.findById(id);
 
-    return res.render("edit", { pageTitle: `Editing` });
+    if (!video) {
+        return res.render("404", { pageTitle: "Video not found" });
+    }
+
+    return res.render("edit", { pageTitle: `Editing: ${video.title}`, video });
 };
 
-export const postEdit = (req, res) => {
+export const postEdit = async (req, res) => {
     const { id } = req.params;
-    const { title } = req.body;
+    const { title, description, hashtags } = req.body;
+    const video = await Video.exists({ _id: id });
+
+    if (!video) {
+        return res.render("404", { pageTitle: "Video not found" });
+    }
+    await Video.findByIdAndUpdate(id, {
+        title,
+        description,
+        hashtags: Video.formatHashtags(hashtags),
+    });
 
     return res.redirect(`/videos/${id}`);
 };
@@ -29,8 +48,24 @@ export const getUpload = (req, res) => {
     return res.render("upload", { pageTitle: "Upload Video" });
 };
 
-export const postUpload = (req, res) => {
-    const { title } = req.body;
+export const postUpload = async (req, res) => {
+    const { title, description, hashtags } = req.body;
+    try {
+        await Video.create({
+            title,
+            description,
+            hashtags: Video.formatHashtags(hashtags),
+            meta: {
+                views: 0,
+                rating: 0,
+            },
+        });
 
-    return res.redirect("/");
+        return res.redirect("/");
+    } catch (error) {
+        return res.render("upload", {
+            pageTitle: "Upload Video",
+            errorMessage: "All Fields Required",
+        });
+    }
 };
